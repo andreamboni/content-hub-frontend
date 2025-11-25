18290d0c448dff15cb66ae28dc51db495a89ba88
00mimir <00mimir@tuta.io>
2025-11-25 10:01:58 -0300

    adding react with vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { AsoiafList } from "./AsoiafList.jsx";
+
+function App() {
+  return (
+    <>
+      <AsoiafList />
+    </>
+  )
+}
+
+export default App;
---
+++ src/AsoiafList.jsx
@@ -0,0 +1,13 @@
+export function AsoiafList() {
+  return (
+    <ul>
+      <li>A Game of Thrones</li>
+      <li>A Clash of Kings</li>
+      <li>A Storm of Swords</li>
+      <li>A Feast for Crows</li>
+      <li>A Dance with Dragons</li>
+      <li>The Winds of Winter</li>
+      <li>A Dream of Spring</li>
+    </ul>
+  )
+}
