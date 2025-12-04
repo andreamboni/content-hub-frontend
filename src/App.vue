cc22670dfb256dec484f44b091631eefa060efd3
Andre Amboni <andreamboni@192.168.0.2>
2025-12-04 14:50:01 -0300

    introducing vue

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Header from './components/Header.vue';
+import Hero from './components/Hero.vue';
+
+</script>
+
+<template>
+  <Header />
+  <Hero />
+</template>
+
+<style scoped></style>
